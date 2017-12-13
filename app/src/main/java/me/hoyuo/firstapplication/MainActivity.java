71a01182206969af5a7abe8ed42e6b6fc2fddf58
Hoyuo <psh9907@gmail.com>
2017-12-13 23:20:50 +0900

    Test File

--- app/src/main/java/me/hoyuo/firstapplication/MainActivity.java
+++ app/src/main/java/me/hoyuo/firstapplication/MainActivity.java
@@ -1,13 +1,19 @@
 package me.hoyuo.firstapplication;
 
 import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
@@ -17,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
     LottieAnimationView animationView;
     ImageView view;
     CircularProgressBar bar;
-
+    LinearLayout linearLayout;
+    TextView start;
     int num;
 
     @Override
@@ -28,8 +35,9 @@ public class MainActivity extends AppCompatActivity {
         view = findViewById(R.id.test_view);
         bar = findViewById(R.id.progress);
         btn = findViewById(R.id.start_btn);
+        linearLayout = findViewById(R.id.total);
         animationView = findViewById(R.id.animation_view);
-
+        start = findViewById(R.id.start);
         animationView.addAnimatorListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animation) {
@@ -61,7 +69,21 @@ public class MainActivity extends AppCompatActivity {
                 animationView.setAnimation("a.json");
                 animationView.playAnimation();
                 CountTask countTask = new CountTask();
-                countTask.execute((int)animationView.getDuration());
+                countTask.execute((int) animationView.getDuration());
+                start.setVisibility(View.VISIBLE);
+                AnimatorSet animatorSet = new AnimatorSet();
+                ObjectAnimator alphaAnimation = ObjectAnimator.ofFloat(start, View.ALPHA, 0, 1).setDuration(5000);
+                animatorSet.playTogether(alphaAnimation);
+                animatorSet.start();
+                Animation animator = new ScaleAnimation(
+                        1f, 0.65f,
+                        1f, 0.65f,
+                        Animation.RELATIVE_TO_SELF, 0f,
+                        Animation.RELATIVE_TO_SELF, 1f);
+
+                animator.setFillAfter(true);
+                animator.setDuration(5000);
+                linearLayout.startAnimation(animator);
             }
         });
     }
